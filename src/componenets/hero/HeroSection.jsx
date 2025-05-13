ec1884248d90d7e0a824a869f7b237033eadfd5e
anshu-kumar27 <anshukumar3552@gmail.com>
2025-05-13 15:54:07 +0530

    pfp update

--- src/componenets/hero/HeroSection.jsx
+++ src/componenets/hero/HeroSection.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import pfp from '../../assets/pics/pfp.png'
-import './hero.css'
+import pfp from '../../assets/pics/pfp.png';
+import './hero.css';
+
 function HeroSection() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div className="hero min-h-[calc(100vh-6rem)] bg-base-200">
       <div className="hero-content flex-col lg:flex-row w-[85%] mx-auto p-5 text-center lg:text-left">
-        <img
-          src={pfp}
-          className="w-full max-w-xs sm:max-w-sm p-6"
-          alt="pfp"
-          loading="eager"
-        />
+        
+        {/* Image Loader Block */}
+        <div className="relative w-full max-w-xs sm:max-w-sm p-6 flex justify-center items-center">
+          {!imageLoaded && (
+            <div className="w-full h-full flex items-center justify-center">
+              {/* Loader Spinner */}
+              <div className="w-12 h-12 border-4 border-dashed border-primary rounded-full animate-spin"></div>
+            </div>
+          )}
+
+          <img
+            src={pfp}
+            className={`w-full max-w-xs sm:max-w-sm transition-opacity duration-500 ease-in-out ${
+              imageLoaded ? "opacity-100" : "opacity-0 absolute"
+            }`}
+            alt="pfp"
+            onLoad={() => setImageLoaded(true)}
+            loading="eager"
+          />
+        </div>
+
         <div>
           <h1 className="text-2xl sm:text-4xl lg:text-5xl font-bold">
             Hi, I am Anshu Kumar
@@ -32,6 +50,7 @@ function HeroSection() {
               </span>
             </span>
           </h1>
+
           <p className="pt-6 pb-4">
             Passionate about building full-stack applications with delightful user experiences.
           </p>
@@ -47,7 +66,6 @@ function HeroSection() {
             </a>
             <span className="block text-sm text-gray-500">Don't forget to turn on cookies to avoid errors.</span>
           </p>
-
 
           {/* Social Icons */}
           <div className="flex space-x-4 mt-4 justify-center">
