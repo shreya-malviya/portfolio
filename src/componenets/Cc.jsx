a94720959c53c09716fd0734ffbfe11617932afa
anshu-kumar27 <anshukumar3552@gmail.com>
2025-05-25 18:01:23 +0530

    cc

--- src/componenets/Cc.jsx
+++ src/componenets/Cc.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Cc() {
   return (
     <div className="flex items-center justify-center mt-6 mb-10 cursor-default p-10">
-      <span className="text-sm italic"></span>
+      <span className="text-sm italic">Shreya | 2025</span>
     </div>
   );
 }
